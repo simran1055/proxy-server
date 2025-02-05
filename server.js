c1513f4efd89f63dde8023020c8b1c328c72a404
Harsimran Singh <harsimran.sliet@gmail.com>
2025-02-06 02:58:27 +0530

    update

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const replace = require('absolutify')
 
 const app = express()
 
+var headers = {
+    // 'accept-language' : 'en\r\n',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent': 'Mozilla/5.0'
+}
+
 app.get('/', async (req, res) => {
-    const {url} = req.query
-    
+    const { url } = req.query
+
     if (!url) {
         return res.send('Not url provided')
     } else {
@@ -16,19 +22,24 @@ app.get('/', async (req, res) => {
             // const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
             const browser = await puppeteer.launch()
             const page = await browser.newPage()
+            // await page.setRequestInterception(true);
+            await page.setExtraHTTPHeaders({
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+                'x-requested-with': 'XMLHttpRequest',
+            })
             await page.goto(`https://${url}`)
-            
+
             let document = await page.evaluate(() => document.documentElement.outerHTML)
             document = replace(document, `/?url=${url.split('/')[0]}`)
-            
+
             return res.send(document)
-        } catch(err) {
+        } catch (err) {
             console.log(err)
-            
+
             return res.send(err)
         }
     }
 })
 
 
-app.listen(process.env.PORT)
+app.listen(8080)
